fe1f641b56ac208428fb57e1aea3e4c67e1fe154
Freddy Sebastian Chia Vera <freddy.chia@wiedii.co>
2024-10-07 08:19:47 -0500

    text input custom

--- src/screens/inputs/TextInputScreen.tsx
+++ src/screens/inputs/TextInputScreen.tsx
@@ -1,12 +1,52 @@
-import { Text, View } from 'react-native'
+import { Text, TextInput, View } from 'react-native'
 import { CustomView } from '../../presentation/components/ui/CustomView'
 import { Title } from '../../presentation/components/ui/Title'
+import { Card } from '../../presentation/components/ui/Card'
 import { globalStyles } from '../../config/theme/theme'
+import { useState } from 'react'
 
 export const TextInputScreen = () => {
+
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        phone: ''
+    });
+
     return (
         <CustomView margin>
-            <Title text='TextInputScreen' safe></Title>
+            <Title text='Text Inputs' safe></Title>
+            <Card>
+                <TextInput style={globalStyles.input}
+                    placeholder='Enter your name'
+                    autoCapitalize='words'
+                    autoCorrect={false}
+                    onChangeText={value => setForm({...form, name: value})}
+                />
+            </Card>
+
+            <Card>
+                <TextInput style={globalStyles.input}
+                    placeholder='Enter your email'
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    keyboardType='email-address'
+                    onChangeText={value => setForm({...form, name: value})}
+                />
+            </Card>
+
+            <Card>
+                <TextInput style={globalStyles.input}
+                    placeholder='Enter your phone'
+                    keyboardType='phone-pad'
+                    onChangeText={value => setForm({...form, name: value})}
+                />
+            </Card>
+
+        <View style={{height: 10}}/>
+            <Card>
+                <Text>{JSON.stringify(form, null, 2)}</Text>
+            </Card>
         </CustomView>
     )
 }

b26ba140f52201bae905d2cbc661d2dc9b13ab27
Freddy Sebastian Chia Vera <freddy.chia@wiedii.co>
2024-11-22 15:13:10 -0500

    slides screen finalized

--- src/presentation/components/ui/Button.tsx
+++ src/presentation/components/ui/Button.tsx
@@ -17,7 +17,7 @@ export const Button = ({ text, styles, onPress }: Props) => {
                 {
                     opacity: pressed ? 0.8 : 1,
                     backgroundColor: colors.primary,
-                }
+                }, styles
             ])}
         >
             <Text style={[
--- src/presentation/components/ui/SlidesScreen.tsx
+++ src/presentation/components/ui/SlidesScreen.tsx
@@ -1,6 +1,9 @@
-import { ImageSourcePropType, Text, View } from 'react-native'
-import { colors } from '../../../config/theme/theme';
+import { Image, ImageSourcePropType, NativeScrollEvent, NativeSyntheticEvent, Text, useWindowDimensions, View } from 'react-native'
+import { colors, globalStyles } from '../../../config/theme/theme';
 import { FlatList } from 'react-native-gesture-handler';
+import { Button } from './Button';
+import { useRef, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 interface Slide {
     title: string;
@@ -27,18 +30,88 @@ interface Slide {
   ];
 
 export const SlidesScreen = () => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                backgroundColor: colors.background,
-            }}
-        >
-            <FlatList
-                data = {items}
-                keyExtractor={(item) => item.title}
-                renderItem={({item}) => <Text> {item.title} </Text>}
+
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+  const flatListRef = useRef<FlatList>(null);
+  const navigation = useNavigation();
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    const { contentOffset, layoutMeasurement } = event.nativeEvent;
+    const currentIndex = Math.floor(contentOffset.x / layoutMeasurement.width);
+    setCurrentSlideIndex(currentIndex > 0 ? currentIndex : 0);
+  }
+
+  const scrollToSlide = (index: number) => {
+    if( !flatListRef.current ) return;
+
+    flatListRef.current.scrollToIndex({
+      index,
+      animated: true,
+    })
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.background,
+      }}
+    >
+      <FlatList
+        ref={flatListRef}
+        data={items}
+        keyExtractor={(item) => item.title}
+        renderItem={({ item }) => <SlideItem item = {item}/>}
+        horizontal
+        pagingEnabled
+        // decelerationRate={'fast'}
+        scrollEnabled={false}
+        onScroll={(ev) => { onScroll(ev) }}
+      />
+      {
+        currentSlideIndex === items.length - 1
+          ?
+          (
+            <Button
+              text="Finalizar"
+              styles={{ position: 'absolute', bottom: 60, right: 30, width: 100 }}
+              onPress={() => navigation.goBack()}
             />
-        </View>
-    )
+          )
+          :
+          (
+            <Button
+              text="Siguiente"
+              styles={{ position: 'absolute', bottom: 60, right: 30, width: 100 }}
+              onPress={() => scrollToSlide(currentSlideIndex + 1)}
+            />
+          )
+      }
+    </View>
+  )
+}
+
+interface SlideItemProps {
+  item: Slide;
+}
+
+const SlideItem = ({ item } : SlideItemProps) => {
+
+  const { width } = useWindowDimensions();
+  const { title, desc, img } = item;
+
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: 'white',
+      borderRadius: 5,
+      padding: 40,
+      justifyContent: 'center',
+      width: width,
+    }}>
+      <Image source={img} style={{ width:  width * 0.7, height: width * 0.7, resizeMode: 'center', alignSelf: 'center' }} />
+      <Text style={[globalStyles.title, {color: colors.primary}]}>{title}</Text>
+      <Text style={{color: colors.text, marginTop: 20}}>{desc}</Text>
+    </View>
+  )
 }
